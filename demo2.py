f7f945441342595146fd2cb98ce2291cdd2a00ae
Glisseo <haru556549@outlook.com>
2024-03-20 22:36:56 +0800

    1

---
+++ demo2.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python
+# _*_ coding: utf-8 _*_
+
+# 这是一行注释
+print("aaaaa")
---
+++ demo5.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# _*_ coding: utf-8 _*_
+
+name = 'Zed A. Shaw'
+age = 35
+height = 183
+weight = 75
+
+print("My name is %s." % name)
+print("My age is %d." % age)
+print("My height is %d." % height)
+print("My weight is %d." % weight)
